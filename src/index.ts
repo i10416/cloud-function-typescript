8b32f60d8dd74907ee2a8567a1d5eb8a4eb0a066
ItoYo16u <ito.yo16uh90616@gmail.com>
2021-03-18 23:39:37 +0900

    add auth setup

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,12 @@ import { google } from "googleapis";
 
 const sqlAdmin = google.sqladmin("v1beta4");
 
+const setup = async () => {
+  const auth = new google.auth.GoogleAuth({ scopes: [] });
+  const authClient = await auth.getClient();
+  google.options({ auth: authClient });
+}
+
 export const helloHTTPFunction = async (req: Request, res: Response) => {
   console.log(req);
   res.send();
@@ -11,5 +17,6 @@ export const helloHTTPFunction = async (req: Request, res: Response) => {
 /// cloud scheduler から PubSubJson に対応する json を payload として渡す
 export const helloPubSubSubscriber = async (json: PubSubJson, context: PubSubContext) => {
   const data = Buffer.from(json.message.data, "base64").toString();
+  sqlAdmin.instances.export
 }
 
